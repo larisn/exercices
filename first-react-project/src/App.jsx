5b53f78d020f00c54f70636433484a9396ebb474
Larissa <contatolarisn@gmail.com>
2023-04-21 13:03:22 -0300

    adicionando exemplos

--- first-react-project/src/App.jsx
+++ first-react-project/src/App.jsx
@@ -1,4 +1,5 @@
 
+import { useEffect, useState } from 'react';
 import Condicional from './components/Condicional';
 import './index.scss'
 // import HelloWorld from "./components/HelloWorld";
@@ -64,11 +65,120 @@ import './index.scss'
 // export default App;
 
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Renderização condicional</h1>
+//       <Condicional />
+//     </div>
+//   )
+// }
+
+// export default App;
+
+
+// function App() {
+//   const [valueInput, setValueInput] = useState('Larissa')
+//   const onChangeInput = (e) => {
+//     setValueInput(e.target.value)
+//   }
+//   return (
+//     <div>
+//       <div className="App">
+//         <input onChange={onChangeInput}></input>
+//       </div>
+//       <h3>{valueInput}</h3>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+
+// function App() {
+//   const [count, setCount] = useState(0)
+//   // let count = 0
+//   const add = () => {
+//     setCount(count + 1)
+//     console.log(count)
+//     /* count = count + 1 (por ser uma 
+//     constante não funciona assim) */
+//   }
+// }
+
+// export default App;
+
+
+// function App() {
+
+//   const [count, setCount] = useState(0)
+
+
+//   // vai renderizar sempre
+//   useEffect(() => {
+//     console.log('Renderiza Sempre')
+//   })
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       console.log(`O count foi executado ${count}`)
+//     }, 1000)
+//     return () => {
+//       clearTimeout(timer)
+//     }
+//   }, [count])
+
+//   useEffect(() => {
+//     console.log('Renderiza na primeira vez que a página é carregada')
+//   }, []) // array de dependência - depende do array pra fazer alguma coisa
+//   // dentro desse array podemos controlar quando o useEffect será disparado
+
+
+//   return (
+//     <div className="App">
+//       <div className="card">
+//         <p>Foi renderizado {count}</p>
+//         <button onClick={() => setCount((count) => count + 1)}>Render</button>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+
+// Request API
+
 function App() {
+
+  const [data, setData] = useState("")
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/larisn/repos')
+    .then((res) => res.json())
+    .then((data) => setData(data))
+  }, [])
+
+  /* Como o data é um dado assíncrono, demora um pouco pra carregar, então 
+  é bom colocar algum if. 
+
+  Também para evitar do data ser undefined quando o código ler o data do h3 
+  porque ele está começando com uma string vazia no useEffect, então ele só
+  seria renderizado quando o data passasse a existir
+  
+  */
+
+  if (!data) {
+    return <p>loading</p>
+  }
   return (
     <div className="App">
-      <h1>Renderização condicional</h1>
-      <Condicional />
+      {/* <h3>Name: {data.name}</h3> */}
+      <h3>Repos: {data.map((repo) => repo.full_name)}</h3>
+      <div className="card">
+        <p>Foi renderizado {count}</p>
+        <button onClick={() => setCount((count) => count + 1)}>Render</button>
+      </div>
     </div>
   )
 }
